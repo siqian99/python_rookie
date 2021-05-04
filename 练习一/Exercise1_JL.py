8e91a6700fe4f65c84c0a2efee0b2e88680df67a
Mr-4000 <wangzch99@gmail.com>
2021-05-05 03:04:09 +0800

    1

---
+++ 练习一/Exercise1_JL.py
@@ -0,0 +1,16 @@
+#1
+s1="MT"
+s2=10
+s3="su"
+s4=5
+pi=3.141592653
+print(s1+s3)
+print(s2+s4)
+print('%10.3f'%pi)
+#2
+print("my_age = 18 # 女孩子永远18")
+print("my_eyes = brown")
+#3
+print('\"To be or no to be,\" \nHamlet said,\n\"that is a question\"')
+#4
+print(1+2,2+3,3-4,4/5,5*6%7)
