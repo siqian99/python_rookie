0cb5a7cbb66bb4c814c316086c6e7893977891d8
Mr-4000 <wangzch99@gmail.com>
2021-06-02 00:58:57 +0800

    x

--- 练习五/1.py
+++ 练习五/1.py
@@ -1,8 +1,7 @@
-f1 = open("Score2.txt", 'r')
-f2 = f1.readline()
-while (f2 != None):
-    if (f2 >= "60"):
-        f3 = open("Grade3.txt", 'w')
-        f3.write(f2)
-        exit(0)
-    f2 = f1.readline()
+a = 666
+list = []
+while (a!=0):
+    list.append(a % 2)
+    a = a // 2
+for i in range(0,len(list)):
+    print(list[len(list)-i-1],end="")
