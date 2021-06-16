5eda5a02b13b10af21ba876f076f651e84d71d15
Mr-4000 <wangzch99@gmail.com>
2021-06-16 08:14:15 +0800

    1

--- 练习五/1.py
+++ 练习五/1.py
@@ -1,7 +1,7 @@
 a = 666
 list = []
 while (a!=0):
-    list.append(a % 2)
-    a = a // 2
+    list.append(a % 8)
+    a = a // 8
 for i in range(0,len(list)):
     print(list[len(list)-i-1],end="")
